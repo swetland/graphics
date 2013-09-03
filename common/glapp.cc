e722bb8b90ed644d42513e05a863ee19b4b7bde8
Brian Swetland <swetland@frotz.net>
2013-09-02 23:57:14 -0700

    build: fix win7 build

--- common/glapp.cc
+++ common/glapp.cc
@@ -160,8 +160,8 @@ static const char *dbg_severity(GLenum n) {
 	default: return "unknown";
 	}
 }
-static void dbg_error_callback(GLenum source, GLenum type, GLenum id, GLenum severity,
-	GLsizei length, const char *message, void *cookie) {
+static void APIENTRY dbg_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
+	GLsizei length, const GLchar *message, GLvoid *cookie) {
 #if VERBOSE
 	error("%s: %s: %x: %s: %s",
 		dbg_source(source), dbg_type(type), id,
@@ -216,11 +216,11 @@ int App::start(void) {
 	gl_map_functions();
 
 	{ // TODO: filter or disable in release mode
-		PFNGLDEBUGMESSAGECALLBACKPROC fn;
-		fn = (PFNGLDEBUGMESSAGECALLBACKPROC)
+		PFNGLDEBUGMESSAGECALLBACKARBPROC fn;
+		fn = (PFNGLDEBUGMESSAGECALLBACKARBPROC)
 			SDL_GL_GetProcAddress("glDebugMessageCallbackARB");
 		if (fn) {
-			fn(dbg_error_callback, NULL);
+			fn(dbg_callback, NULL);
 			glEnable(GL_DEBUG_OUTPUT);
 			// glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
 		} else {
